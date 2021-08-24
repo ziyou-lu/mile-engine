0a2b48a2dbd97cc0dd375a62d9b8c233e1965213
ziyoulu <ziyoulu1993@163.com>
2021-08-24 17:51:27 +0800

    【add】rbatis初始化
    修改说明: rbatis初始化
    配置更改: init.toml
    脚本更改: 无
    需求/缺陷来源: 主线版本
    平台/产品/模块: 全局
    波及分析: 无
    自测结果: 编译通过，还未自测

---
+++ src/config/init_config.rs
@@ -0,0 +1,46 @@
+use toml;
+use std::fs::File;
+use std::io::Read;
+use serde_derive::Deserialize;
+
+#[derive(Deserialize)]
+pub struct InitConfig {
+    pub global: GlobalConfig,
+    pub db: DBConfig,
+}
+
+#[derive(Deserialize)]
+pub struct GlobalConfig {
+    pub host: String,
+    pub debug: bool,
+}
+
+#[derive(Deserialize)]
+pub struct DBConfig {
+    pub mysql_url: String,
+    pub logic_column: String,
+    pub logic_un_deleted: i64,
+    pub logic_deleted: i64,
+}
+
+impl Default for InitConfig {
+    fn default() -> Self {
+        let file_path = "../../res/init.toml";
+        let mut file = match File::open(file_path) {
+            Ok(f) => f,
+            Err(e) => panic!("no such file: {}, error: {}", file_path, e)
+        };
+
+        let mut file_content = String::new();
+        if let Err(e) = file.read_to_string(&mut file_content) {
+            panic!("read file context error! file: {} error: {}", file_path, e);
+        }
+
+        let init_config = match toml::from_str(&file_content) {
+            Ok(c) => c,
+            Err(e) => panic!("load init config to cache error, file: {}, error: {}", file_path, e)
+        };
+
+        init_config
+    }
+}
---
+++ src/db/mod.rs
@@ -0,0 +1,24 @@
+use crate::InitConfig;
+use rbatis::plugin::logic_delete::{RbatisLogicDeletePlugin};
+use rbatis::rbatis::Rbatis;
+
+pub mod mapper;
+
+pub async fn init_rbatis(config: &InitConfig) -> Rbatis {
+    let mut rbatis = Rbatis::new();
+    //logic plugin 设置逻辑删除插件
+    rbatis.logic_plugin = Some(Box::new(RbatisLogicDeletePlugin::new_opt(
+        &config.db.logic_column,
+        config.db.logic_deleted as i32,
+        config.db.logic_un_deleted as i32,
+    )));
+    if config.global.debug.eq(&false) && rbatis.is_debug_mode() {
+        panic!(
+            r#"已使用release模式，但是rbatis仍使用debug模式！请删除 Cargo.toml 中 rbatis的配置 features = ["debug_mode"]"#
+        );
+    }
+    //连接数据库
+    rbatis.link(&config.db.mysql_url).await.expect("[abs_admin] rbatis link database fail!");
+    println!("[abs_admin] rbatis link database success!");
+    return rbatis;
+}
